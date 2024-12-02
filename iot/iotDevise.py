9052cd0bec86e58b43e793871cb09b87ff108bf0
Marcel <mauer@vivaldi.net>
2024-12-02 17:54:29 +0100

    added logic to chose a random video and get frames of it

--- iot/iotDevise.py
+++ iot/iotDevise.py
@@ -1,9 +1,21 @@
-'''class for handeling the iot devices'''
-
-import json
 import random
 import os
 import cv2
+import time 
+import threading
+
+frame_rate = 24 # TODO: find out the frame rate of the videos
+secounds_between_images = 5
+delay_images = frame_rate * secounds_between_images # 5 seconds delay between each image
+
+def send_frame(frame):
+    # TODO: send the frame to the edge layer to be processed
+    pass
+
+# @app.route('/trigger_alarm', methods=['POST'])
+def waiting_for_alarm():
+    # TODO: implement logic that waits for an alarm from the AWS and prints something
+    pass
 
 def main(randomGenerator):
     path_to_video = "data//wisenet_dataset/video_sets/set_"
@@ -12,28 +24,28 @@ def main(randomGenerator):
         randomSet = randomGenerator.randint(1, 11) 
         # selects all videos from the chosen set
         videos = os.listdir(path_to_video + str(randomSet))
-        # selects one of the videos randomly
+        # selects all videos from the chosen set 
         for video in videos:
             cap = cv2.VideoCapture(path_to_video + str(randomSet) + "//" + video)
+            counter = 0
             while(cap.isOpened()):
                 ret, frame = cap.read()
-                if ret:
-                    # Display the resulting frame
-                    cv2.imshow('frame',frame)
-                    if cv2.waitKey(1) & 0xFF == ord('q'):
-                        break
-                else:
+                if not ret: 
                     break
+                counter += 1
+                # we only need to send the frame every 5 seconds
+                if (counter > delay_images):
+                    # Display the resulting frame TODO remove this line after testing
+                    cv2.imshow('frame',frame)
+                    counter = 0
+                    # send the frame
+                    send_frame(frame)
+                    # waits for 5 seconds to simulate the delay
+                    time.sleep(secounds_between_images)
+            print("Video ended")
             cap.release()
             cv2.destroyAllWindows()
         
-        print("Video set " + str(randomSet) + " is done")
-        return
-        
-
-
-
-
 if __name__ == "__main__":
     # get the environment variable for the seed
     seed = os.environ.get('SEED', 111)
