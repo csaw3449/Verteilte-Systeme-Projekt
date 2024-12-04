daae4dd6d60f479e6bcc28d58b36d5cfc52e88c2
Roman <roman.jarz@student.uibk.ac.at>
2024-12-04 16:59:00 +0100

    Made it multithreadable (sender, receiver) and startet implementing a sns for the communication to the edge layer.

--- iot/iotDevise.py
+++ iot/iotDevise.py
@@ -3,21 +3,46 @@ import os
 import cv2
 import time 
 import threading
-
-frame_rate = 24 # TODO: find out the frame rate of the videos
+import boto3
+import json
+"""
+This programm sends images in from a video dataset to the edge layer in a specific time interval.
+It also waits for an alarm from the edge layer and prints it.
+This programm need environment variables to be set:
+- IOT_ID: the id of the iot device
+- SET_NUMBER: the number of the video set to be used
+"""
+frame_rate = 24 # TODO: find out the frame rate of the videos, not really that important. Could also make random sampling
 secounds_between_images = 5
 delay_images = frame_rate * secounds_between_images # 5 seconds delay between each image
+#aws communicatioin
+sqs = boto3.resource('sqs', region_name='us-east-1')
+send_queue = sqs.get_queue_by_name(QueueName='images')  #TODO: names might change, check with edge layer
+receive_queue = sqs.get_queue_by_name(QueueName='alarm')
+
+id = os.environ.get('IOT_ID')
 
 def send_frame(frame):
     # TODO: send the frame to the edge layer to be processed
-    pass
+    message = {
+        "iot_id": id,
+        "frame": cv2.imencode('.jpg', frame)[1].tobytes()
+    }
+    send_queue.send_message(MessageBody=json.dumps(message))
+    print(f"Frame sent frame to queue: {message}")
 
 # @app.route('/alarm', methods=['POST'])
 def waiting_for_alarm():
     # TODO: implement logic that waits for an alarm from the AWS and prints something
-    pass
+    while True:
+        response = receive_queue.receive_messages(MaxNumberOfMessages=1, WaitTimeSeconds=10)
+        for message in response:
+            body = json.loads(message.body)
+            if body.get("iot_id") == id:
+                print(f"Alarm received: {message.body}")
+                message.delete()
 
-def main():
+def send_images():
     set_number = os.environ.get('SET_NUMBER', 1)
     path_to_video = "data//wisenet_dataset/video_sets/set_"
     while True: 
@@ -46,6 +71,13 @@ def main():
                     break
             cap.release()
             cv2.destroyAllWindows()
+
+def main():
+    threadSend = threading.Thread(target=send_images)
+    threadReceive = threading.Thread(target=waiting_for_alarm)
+    
+    threadSend.start()
+    threadReceive.start()
         
 if __name__ == "__main__":
     main()
