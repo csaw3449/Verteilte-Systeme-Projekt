935cc0098d913c7076486cd45cea8dbea67ffe7c
Marcel <mauer@vivaldi.net>
2024-12-05 15:47:04 +0100

    added edge.py for edge layer

---
+++ edge/edge.py
@@ -0,0 +1,18 @@
+import boto3
+import botocore
+import time
+
+MAX_RETRIES = 5
+
+#Function to get the queue, with MAX_RETRIES attemps
+
+async def main():
+    while True:
+        # Receive messages from the load balancer
+        # TODO: Implement this
+        frame = NotImplementedError
+        # Process frame using YOLO
+
+
+if __name__ == "__main__":
+    main()
