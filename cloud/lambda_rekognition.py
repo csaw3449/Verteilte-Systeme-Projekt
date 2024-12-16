8ffc97f2b4b2ca46cac0b322791cfe4bd57f3b4c
Marcel <mauer@vivaldi.net>
2024-12-16 11:54:41 +0100

    added lambdafunction, which compares the image to the faces in the collection, needs to be tested

---
+++ cloud/lambda_rekognition.py
@@ -0,0 +1,42 @@
+import boto3
+
+rekognition = boto3.client('rekognition')
+
+# this lambda function is triggered by the edge layer and the pictures a located in a container
+def lambda_handler(event, context):
+    # get the image and iot_id from the event
+    image = event['image']
+    iot_id = event['iot_id']
+
+    # look for similar faces in the collection and check if there is a 90% match
+    response = rekognition.search_faces_by_image(
+        CollectionId='pfusch-collection',
+        Bucket='pfusch-bucket',
+        Image={
+            'Bytes': image
+
+        },
+        FaceMatchThreshold=90
+    )
+
+    # if there is a match, return the iot_id
+    if len(response['FaceMatches']) > 0:
+        return {
+            'status': 'known',
+            'iot_id': iot_id
+        }
+    else:
+        return {
+            'status': 'unknown'
+            'iot_id': iot_id
+        }
+    
+
+
+
+
+    
+
+
+
+    
