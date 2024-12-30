b471f8cef90e77b1881021cf8c613d426d89732a
Andreas Voppichler <Andreas.Voppichler@student.uibk.ac.at>
2024-12-30 14:16:44 +0100

    Updated edge.py adn terraform + start.sh

--- terraform_edge/start.sh
+++ terraform_edge/start.sh
@@ -3,15 +3,9 @@ host=$(terraform show | grep public_dns | sed -n 's/.*"\([^"]*\)".*/\1/p')
 
 # Install Docker dependencies on EC2
 # ssh -o "StrictHostKeyChecking=accept-new" -i "EC2-key.pem" ec2-user@$host sudo yum install python3-pip -y
-ssh -i "EC2-key.pem" ec2-user@$host sudo yum install -y docker
-ssh -i "EC2-key.pem" ec2-user@$host sudo service docker start
-ssh -i "EC2-key.pem" ec2-user@$host sudo usermod -a -G docker ec2-user
-ssh -i "EC2-key.pem" ec2-user@$host sudo systemctl enable docker
-ssh -i "EC2-key.pem" ec2-user@$host sudo docker load -i edge.tar
-ssh -i "EC2-key.pem" ec2-user@$host sudo curl -L "https://github.com/docker/compose/releases/download/v2.21.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-ssh -i "EC2-key.pem" ec2-user@$host sudo chmod +x /usr/local/bin/docker-compose
-ssh -i "EC2-key.pem" ec2-user@$host sudo docker-compose up -d
-
+ssh -i "EC2-key.pem" ec2-user@$host sudo yum install python3-pip -auto-approve
+ssh -i "EC2-key.pem" ec2-user@$host pip3 install boto3 opencv-python-headless numpy botocore
+ssh -i "EC2-key.pem" ec2-user@$host mkdir yolo3
 
 # Connect to ssh
 ssh -i "EC2-key.pem" ec2-user@$host 
