40d2791724fe034b48dd0b3b9ad5aa9959b8652f
Marcel <mauer@vivaldi.net>
2024-12-30 13:49:50 +0100

    fixed init_conatainer in cloud

--- cloud/init_container.py
+++ cloud/init_container.py
@@ -1,14 +1,11 @@
-import logging as logger
 from botocore.exceptions import ClientError
 import os
 import boto3
-# Create a logger
-logger = logger.getLogger(__name__)
 
 try: 
     prof_name =os.getenv('PROFILE_NAME')
 except KeyError:
-    logger.error('Please set the profile name')
+    print('Please set the profile name')
     exit(1)
 
 def create_collection(bucket, collection_id, path_to_images):
@@ -17,7 +14,7 @@ def create_collection(bucket, collection_id, path_to_images):
 
     try:
         client.describe_collection(CollectionId=collection_id)
-        logger.log('Collection already exists')
+        print('Collection already exists')
     except ClientError as e:
         # Create a collection
         print('Creating collection:' + collection_id)
@@ -26,16 +23,20 @@ def create_collection(bucket, collection_id, path_to_images):
             print('Collection ARN: ' + response['CollectionArn'])
             print('Status code: ' + str(response['StatusCode']))
             print('Done...')
+            # write the information to the CollectionConfig file
+            with open('CollectionConfig', 'w') as file:
+                file.write('CollectionId=' + collection_id + '\n')
+                file.write('CollectionArn=' + response['CollectionArn'] + '\n')
+                file.write('Bucket=' + bucket + '\n')
         except ClientError as e:
-            logger.error(e)
+            print(e)
             return False
 
     # read images from a given path and add them to the collection
     with os.scandir(path_to_images) as entries:
         for entry in entries:
             if entry.is_file():
-                print("Adding image: " + entry.name)
-                indexed_faces_count = client.index_faces(Collection_id=collection_id,
+                indexed_faces_count = client.index_faces(CollectionId=collection_id,
                 Image={'S3Object': {'Bucket': bucket, 'Name': entry.name}},
                                   ExternalImageId=entry.name,
                                   MaxFaces=1,
@@ -50,21 +51,31 @@ def create_bucket(bucket_name):
     s3 = session.client('s3')
     try:
         s3.head_bucket(Bucket=bucket_name)
-        logger.log('Bucket already exists')
+        print('Bucket already exists')
         return True
     except ClientError as e:
         try:
             s3.create_bucket(Bucket=bucket_name)
         except ClientError as e:
-            logger.error(e)
+            print(e)
             return False
-        logger.log('Bucket created')
+        print('Bucket created')
         return True
+
+def add_images_to_bucket(bucket_name, path_to_images):
+    session = boto3.Session(profile_name=prof_name, region_name='us-east-1')
+    s3 = session.client('s3')
+    with os.scandir(path_to_images) as entries:
+        for entry in entries:
+            if entry.is_file():
+                s3.upload_file(path_to_images + '/' + entry.name, bucket_name, entry.name)
+                print('Image uploaded to bucket: ' + entry.name)
 
 if __name__ == '__main__':
     path_to_images = os.getenv('PATH_TO_IMAGES')
     if path_to_images is None:
-        logger.error('Please set the path to the images')
+        print('Please set the path to the images')
         exit(1)
     create_bucket('pfusch-bucket')
+    add_images_to_bucket('pfusch-bucket', path_to_images)
     create_collection('pfusch-bucket', 'pfusch-collection', path_to_images)
