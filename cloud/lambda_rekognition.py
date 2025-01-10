0a16e6778b6e8940a392bcdbbaef2cea95f412ad
Marcel <mauer@vivaldi.net>
2025-01-10 09:54:45 +0100

    pfusched the encoding, hope it works

--- cloud/lambda_rekognition.py
+++ cloud/lambda_rekognition.py
@@ -14,31 +14,36 @@ def lambda_handler(event, context):
     
     # decode the image
     image_decoded = image.encode('latin1')
-    image = cv2.imdecode(np.frombuffer(image_decoded, np.uint8), cv2.IMREAD_COLOR)
-    if image is None:
+    image_buffer = cv2.imdecode(np.frombuffer(image_decoded, np.uint8), cv2.IMREAD_COLOR)
+    if image_buffer is None:
         return {
             'status': 'error',
             'iot_id': iot_id
         }
-    
     # Re-encode the image to raw binary bytes
-    _, image_buffer = cv2.imencode('.jpg', image)
+    # _, image_buffer = cv2.imencode('.jpg', image)
     image_bytes = image_buffer.tobytes()
     
     # Re-encode the image to base 64
     #_, image_buffer = cv2.imencode('.jpg', image)
-    #image64 = base64.b64encode(image_buffer)
+    image64 = base64.b64encode(image_bytes)
     # convet the image to bytes
     #image64 = image64.encode('utf-8')
+    # error handling
+    if image64 is None:
+        return {
+            'status': 'error',
+            'iot_id': iot_id
+        }
 
     # look for similar faces in the collection and check if there is a 90% match
     response = rekognition.search_faces_by_image(
         CollectionId='pfusch-collection',
         QualityFilter='NONE',
         Image={
-            'Bytes': image_bytes
+            'Bytes': image64
         },
-        FaceMatchThreshold=90
+        FaceMatchThreshold=70
     )
 
     # if there is a match, return the status known otherwise unknown
